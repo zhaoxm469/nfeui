9f44e000a1ae4720ea97cb5312e9f0bb677e9f47
zhaoxingming <zhaoxingming@nucarf.com>
2022-01-27 12:36:00 +0800

    test: 解析路径ts设置为第一

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,18 @@
 module.exports = {
-  preset: 'ts-jest',
-  collectCoverageFrom: ['src/**/*.ts'],
-  moduleNameMapper: {},
-  transform: {
-    '^.+\\.vue$': 'vue-jest',
-  },
-  moduleFileExtensions: ['vue', 'js', 'json', 'jsx', 'ts', 'tsx', 'node'],
+    preset: 'ts-jest',
+    collectCoverageFrom: ['src/**/*.ts'],
+    moduleNameMapper: {},
+    transform: {
+        '^.+\\.vue$': 'vue-jest'
+    },
+    moduleFileExtensions: [
+        'ts',
+        'tsx',
+        'vue',
+        'js',
+        'json',
+        'jsx',
+        'tsx',
+        'node'
+    ]
 };
