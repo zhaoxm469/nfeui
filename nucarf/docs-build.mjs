34b131bdbaa77c68ee75842566d56e0ffa7e7d3f
zhaoxingming <zhaoxingming@nucarf.com>
2022-01-28 10:15:12 +0800

    fix: 打包文档导航+1

--- nucarf/docs-build.mjs
+++ nucarf/docs-build.mjs
@@ -1,24 +1,9 @@
 #!/usr/bin/env zx
-
-const { fs, path, chalk } = require('zx');
-const vitepressPath = path.join(__dirname, '../docs/.vitepress/');
-const vitepressConfigPath = path.join(vitepressPath, 'config.js');
-const tempVitepressConfigPath = path.join(
-    __dirname,
-    '../docs/.temp/.vitepress/config.js'
-);
-
-await $`rimraf docs/dist`;
-const rawVitepressData = fs.readFileSync(vitepressConfigPath, 'utf-8');
-const tempVitepressData = fs.readFileSync(tempVitepressConfigPath, 'utf-8');
-fs.writeFileSync(vitepressConfigPath, tempVitepressData);
+const { chalk } = require('zx');
 await $`cross-env NODE_ENV=production vitepress-fc build --root=docs`;
-fs.writeFileSync(vitepressConfigPath, rawVitepressData);
 await $`clear`;
-
 console.log(`\n`);
 console.log(
     `打包完成 请输入 ${chalk.blue('npm run docs-serve')} 命令可进行打包文件预览`
 );
 console.log(`\n`);
-process.exit();
