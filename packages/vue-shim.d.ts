27d1674ba5003f2015c484b1c2b9d68cbc7a0f9c
zhaoxingming <zhaoxingming@nucarf.com>
2022-01-28 10:45:00 +0800

    chore: 删除无必要注释

--- packages/vue-shim.d.ts
+++ packages/vue-shim.d.ts
@@ -1,7 +1,4 @@
 declare module '*.vue' {
-    // import { ComponentOptions } from 'vue';
-    // const comp: ComponentOptions;
-    // export default comp;
     import type { DefineComponent } from 'vue';
     const component: DefineComponent<{}, {}, any>;
     export default component;
