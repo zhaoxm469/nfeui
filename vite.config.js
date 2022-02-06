117082548d8f6edaa97f84a6269c7ec41c477a3a
zhaoxingming <zhaoxingming@nucarf.com>
2022-02-06 12:05:34 +0800

    ci: 打包配置修改

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
 // yarn build 用到的vite配置
-require('dotenv/config');
-import vue from '@vitejs/plugin-vue';
-import vueJsx from '@vitejs/plugin-vue-jsx';
-import { resolve } from 'path';
-import config from './package.json';
+require("dotenv/config");
+import vue from "@vitejs/plugin-vue";
+import vueJsx from "@vitejs/plugin-vue-jsx";
+import { resolve } from "path";
+import config from "./package.json";
 
 const { UI_NAME } = process.env;
 
@@ -12,55 +12,58 @@ const banner = `/*!
 * (c) 2022 @nucarf.
 */`;
 
-import AutoImport from 'unplugin-auto-import/vite';
-import Components from 'unplugin-vue-components/vite';
-import { ElementPlusResolver } from 'unplugin-vue-components/resolvers';
+import AutoImport from "unplugin-auto-import/vite";
+// import Components from 'unplugin-vue-components/vite';
+// import { ElementPlusResolver } from 'unplugin-vue-components/resolvers';
+import ElementPlus from "unplugin-element-plus/vite";
 
 /**
  * @type {import('vite').UserConfig}
  */
 export default {
-    optimizeDeps: {
-        include: [],
-        exclude: []
-    },
-    plugins: [
-        vue(),
-        vueJsx()
-        // AutoImport({
-        //     resolvers: [ElementPlusResolver()],
-        // }),
-        // Components({
-        //     resolvers: [ElementPlusResolver()],
-        // }),
-    ],
-    build: {
-        minify: true,
-        terserOptions: {
-            compress: {
-                drop_console: true,
-                drop_debugger: true
-            }
-        },
-        lib: {
-            entry: resolve('./packages/index.ts'),
-            name: UI_NAME.toLocaleLowerCase(),
-            fileName: UI_NAME.toLocaleLowerCase(),
-            formats: ['es', 'umd']
-        },
-        rollupOptions: {
-            // make sure to externalize deps that shouldn't be bundled
-            // into your library
-            external: ['vue'],
-            output: {
-                banner,
-                // Provide global variables to use in the UMD build
-                // for externalized deps
-                globals: {
-                    vue: 'Vue'
-                },
-                exports: 'named'
-            }
-        }
-    }
+	optimizeDeps: {
+		include: [],
+		exclude: [],
+	},
+	plugins: [
+		vue(),
+		vueJsx(),
+		ElementPlus(),
+		// AutoImport({
+		//     resolvers: [ElementPlusResolver()],
+		// }),
+		// Components({
+		//     resolvers: [ElementPlusResolver()],
+		// }),
+	],
+	build: {
+		minify: true,
+		terserOptions: {
+			compress: {
+				drop_console: true,
+				drop_debugger: true,
+			},
+		},
+		lib: {
+			entry: resolve("./packages/index.ts"),
+			name: UI_NAME.toLocaleLowerCase(),
+			fileName: UI_NAME.toLocaleLowerCase(),
+			formats: ["es", "umd"],
+		},
+		rollupOptions: {
+			// make sure to externalize deps that shouldn't be bundled
+			// into your library
+			external: ["vue", "element-plus"],
+			output: {
+				banner,
+				// Provide global variables to use in the UMD build
+				// for externalized deps
+				globals: {
+					vue: "Vue",
+					"element-plus": "ElementPlus",
+				},
+				exports: "named",
+			},
+		},
+	},
 };
