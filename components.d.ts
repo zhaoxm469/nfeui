11af78adb8ed39d959412e8774bb9885c79c5481
zhaoxingming <zhaoxingming@nucarf.com>
2022-02-06 14:02:54 +0800

    chore: 饿了么组件代码提示

--- components.d.ts
+++ components.d.ts
@@ -2,10 +2,23 @@
 // We suggest you to commit this file into source control
 // Read more: https://github.com/vuejs/vue-next/pull/3399
 
-declare module 'vue' {
-  export interface GlobalComponents {
-    ElButton: typeof import('element-plus/es')['ElButton']
-  }
+declare module "vue" {
+	export interface GlobalComponents {
+		ElButton: typeof import("element-plus/es")["ElButton"];
+		ElCheckbox: typeof import("element-plus/es")["ElCheckbox"];
+		ElCheckboxGroup: typeof import("element-plus/es")["ElCheckboxGroup"];
+		ElCol: typeof import("element-plus/es")["ElCol"];
+		ElDatePicker: typeof import("element-plus/es")["ElDatePicker"];
+		ElForm: typeof import("element-plus/es")["ElForm"];
+		ElFormItem: typeof import("element-plus/es")["ElFormItem"];
+		ElInput: typeof import("element-plus/es")["ElInput"];
+		ElOption: typeof import("element-plus/es")["ElOption"];
+		ElRadio: typeof import("element-plus/es")["ElRadio"];
+		ElRadioGroup: typeof import("element-plus/es")["ElRadioGroup"];
+		ElSelect: typeof import("element-plus/es")["ElSelect"];
+		ElSwitch: typeof import("element-plus/es")["ElSwitch"];
+		ElTimePicker: typeof import("element-plus/es")["ElTimePicker"];
+	}
 }
 
-export { }
+export {};
