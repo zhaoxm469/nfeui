72e5581c5a1eb0cb08fd942760ac0f89b9848a7e
zhaoxingming <zhaoxingming@nucarf.com>
2022-02-06 12:07:27 +0800

    fix: demo class change nt

--- packages/components/button/demo/click.vue
+++ packages/components/button/demo/click.vue
@@ -1,53 +1,53 @@
 <template>
-    <div>
-        <p>
-            按钮点击次数:
-            <span>{{ count }}</span>
-        </p>
-        <div class="tf-flex tf-mt-10">
-            <nfeButton
-                :loading="isLoading"
-                @loading-change="onLoadingChange"
-                type="primary"
-                @click="handleBtnClick"
-                >点我增加次数！</nfeButton
-            >
-        </div>
-    </div>
+	<div>
+		<p>
+			按钮点击次数:
+			<span>{{ count }}</span>
+		</p>
+		<div class="nt-flex nt-mt-10">
+			<nfeButton
+				:loading="isLoading"
+				@loading-change="onLoadingChange"
+				type="primary"
+				@click="handleBtnClick"
+				>点我增加次数！</nfeButton
+			>
+		</div>
+	</div>
 </template>
 
 <script lang="ts" setup>
-import { nfeButton } from 'nfeui';
-import { reactive, toRefs } from 'vue';
+import { nfeButton } from "nfeui";
+import { reactive, toRefs } from "vue";
 
 const useClick = () => {
-    const state = reactive({
-        count: 0,
-        isLoading: false
-    });
+	const state = reactive({
+		count: 0,
+		isLoading: false,
+	});
 
-    const methods = {
-        handleBtnClick() {
-            state.isLoading = true;
-            setTimeout(() => {
-                state.count++;
-                state.isLoading = false;
-            }, 1000);
-        }
-    };
+	const methods = {
+		handleBtnClick() {
+			state.isLoading = true;
+			setTimeout(() => {
+				state.count++;
+				state.isLoading = false;
+			}, 1000);
+		},
+	};
 
-    return {
-        ...toRefs(state),
-        ...methods
-    };
+	return {
+		...toRefs(state),
+		...methods,
+	};
 };
 
 const onLoadingChange = (loading: boolean) =>
-    console.log(`loading 状态发生变化：${loading}`);
+	console.log(`loading 状态发生变化：${loading}`);
 
 const { count, isLoading, handleBtnClick } = useClick();
 </script>
 
 <style scoped lang="scss">
-@import '../../../style/tailwindcss.scss';
+@import "../../../style/tailwindcss.scss";
 </style>
--- packages/components/button/demo/loading.vue
+++ packages/components/button/demo/loading.vue
@@ -1,17 +1,15 @@
 <template>
-    <div class="tf-flex tf-flex-wrap">
-        <nfeButton class="tf-mr-20 tf-mb-10" loading type="primary"
-            >提交</nfeButton
-        >
-        <nfeButton class="tf-mr-20 tf-mb-10" loading>取消</nfeButton>
-        <nfeButton class="tf-mr-20" loading type="success">确认</nfeButton>
-    </div>
+	<div class="nt-flex nt-flex-wrap">
+		<nfeButton class="nt-mr-20 nt-mb-10" loading type="primary">提交</nfeButton>
+		<nfeButton class="nt-mr-20 nt-mb-10" loading>取消</nfeButton>
+		<nfeButton class="nt-mr-20" loading type="success">确认</nfeButton>
+	</div>
 </template>
 
 <script lang="ts" setup>
-import { nfeButton } from 'nfeui';
+import { nfeButton } from "nfeui";
 </script>
 
 <style scoped lang="scss">
-@import '../../../style/tailwindcss.scss';
+@import "../../../style/tailwindcss.scss";
 </style>
