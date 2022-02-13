10cf7a1089e20374856672d068d3b0007db67d6a
zhaoxingming <zhaoxingming@nucarf.com>
2022-02-13 18:28:14 +0800

    chore: 文件抽离

---
+++ packages/components/Form/src/formItemUseElComponent.ts
@@ -0,0 +1,33 @@
+/*
+ * @Author: zhaoxingming
+ * @Date: 2022-02-13 10:42:38
+ * @LastEditTime: 2022-02-13 17:06:11
+ * @LastEditors: vscode
+ * @Description: 这里是formItem 所用的饿了么组件
+ */
+
+export {
+	ElInput,
+	ElFormItem,
+	ElTooltip,
+	ElIcon,
+	ElAutocomplete,
+	ElCol,
+	ElInputNumber,
+	ElSelect,
+	ElOption,
+	ElRadio,
+	ElRadioGroup,
+	ElColorPicker,
+	ElCheckboxGroup,
+	ElRate,
+	ElSlider,
+	ElSwitch,
+	ElUpload,
+	ElTransfer,
+	ElCascader,
+	ElDatePicker,
+	ElTimePicker,
+	ElSelectV2,
+	ElTimeSelect,
+} from "element-plus";
