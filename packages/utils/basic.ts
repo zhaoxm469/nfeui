cfbc3c9a27186dbba7a5067f184062b3af5e42fa
zhaoxingming <zhaoxingming@nucarf.com>
2022-02-13 18:28:44 +0800

    chore: 工具方法

--- packages/utils/basic.ts
+++ packages/utils/basic.ts
@@ -1,1 +1,13 @@
 export const extend = Object.assign;
+
+export function isDef(v: any): boolean {
+	return v !== undefined && v !== null;
+}
+
+export function isPromise(val: any): boolean {
+	return (
+		isDef(val) &&
+		typeof val.then === "function" &&
+		typeof val.catch === "function"
+	);
+}
