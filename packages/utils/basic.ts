b3f0f1cac5439fe165ca874f41b3843448d282ec
zhaoxingming <zhaoxingming@nucarf.com>
2022-02-15 15:41:41 +0800

    chore: add deepEqui Fn

--- packages/utils/basic.ts
+++ packages/utils/basic.ts
@@ -15,3 +15,34 @@ export function isPromise(val: any): boolean {
 export function isFunction(val: Function): boolean {
 	return typeof val === "function";
 }
+
+/*
+ * @param x {Object} 对象1
+ * @param y {Object} 对象2
+ * @return  {Boolean} true 为相等，false 为不等
+ */
+export const deepEqual = (x: any, y: any) => {
+	// 指向同一内存时
+	if (x === y) {
+		return true;
+	} else if (
+		typeof x == "object" &&
+		x != null &&
+		typeof y == "object" &&
+		y != null
+	) {
+		if (Object.keys(x).length !== Object.keys(y).length) {
+			return false;
+		}
+		for (var prop in x) {
+			if (y.hasOwnProperty(prop)) {
+				if (!deepEqual(x[prop], y[prop])) return false;
+			} else {
+				return false;
+			}
+		}
+		return true;
+	} else {
+		return false;
+	}
+};
